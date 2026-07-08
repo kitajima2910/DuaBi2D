e3369abf54144ab6f7f7dd1e8504d1dfce2ac9ef
kitajima2910 <kitajima2910@gmail.com>
2026-07-08 23:39:05 +0700

    refactor: remove unused entities, scenes, and systems
    - Deleted Obstacle entity and its related logic.
    - Removed favicon.svg, index.html, and BootScene, MenuScene, RaceScene, ResultScene.
    - Eliminated CountdownSystem, CountrySystem, EventBus, FinishDetectionSystem, MarbleRenderer, PhysicsSystem, RaceSystem, RankingSystem, SpawnSystem, TrackLoader, and TrackSystem.
    - Cleaned up main.js to reflect the removal of scenes and systems.

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { gameConfig } from '../core/GameConfig.js';
-import { BootScene } from '../scenes/BootScene.js';
-import { MenuScene } from '../scenes/MenuScene.js';
-import { RaceScene } from '../scenes/RaceScene.js';
-import { ResultScene } from '../scenes/ResultScene.js';
-
-gameConfig.scene = [BootScene, MenuScene, RaceScene, ResultScene];
-
-const game = new Phaser.Game(gameConfig);
